080234844ee984308cdf681a136958c02562dcc3
rubone <rubone02@gmail.com>
2013-01-20 11:56:24 -0600

    test

--- MvcApplication/MvcApplication/Models/AccountModels.cs
+++ MvcApplication/MvcApplication/Models/AccountModels.cs
@@ -19,7 +19,7 @@ namespace MvcApplication.Models
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "New password")]
-        public string NewPassword { get; set; }
+        public string NewPassword { get; set; } ///----
 
         [DataType(DataType.Password)]
         [Display(Name = "Confirm new password")]
